00e53100cf0a8f41272b70f6306cfcd9ba901223
Alexander Wake <alec.wake@icloud.com>
2026-03-18 14:44:21 -0500

    Implement full CRUD API with FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,19 +9,23 @@ tasks = [
     {"id": 3, "title": "Practice query params", "completed": False}
 ]
 
+class TaskCreate(BaseModel):
+    title: str
+
+class TaskUpdate(BaseModel):
+    title: str = None
+    completed: bool = None
+
+class TaskReplace(BaseModel):
+    title: str
+    completed: bool
+
 @app.get("/")
 def root():
     return {"current": "Task API is running"}
 
-@app.get("/tasks/{task_id}")
-def get_tasks(task_id: int):
-    for task in tasks:
-        if task["id"] == task_id:
-            return task
-    raise HTTPException(status_code = 404, detail = "Item not Found")
-
 @app.get("/tasks/search")
-def search(completed: bool = None, title = None: str = None):
+def search(completed: bool = None, title: str = None):
     results = []
     if title != None and completed != None:
         for task in tasks:
@@ -39,3 +44,52 @@ def search(completed: bool = None, title = None: str = None):
         return results
     else:
         return tasks
+
+@app.get("/tasks/{task_id}")
+def get_tasks(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    raise HTTPException(status_code = 404, detail = "Item not Found")
+
+@app.post("/tasks")
+def create_task(task: TaskCreate):
+    largest_id = -1
+    if tasks == []:
+        largest_id = 0
+    else:
+        for single_task in tasks:
+            if single_task["id"] > largest_id:
+                largest_id = single_task["id"]
+    tasks.append({"id": largest_id + 1, "title": task.title, "completed": False})
+    return {"id": largest_id + 1, "title": task.title, "completed": False}
+
+@app.delete("/tasks/{id_to_delete}", status_code = 204)
+def delete_task(id_to_delete: int):
+    index = 0
+    for task in tasks:
+        if task["id"] == id_to_delete:
+            del tasks[index]
+            return
+        index += 1
+    raise HTTPException(status_code = 404, detail = "Item not Found")
+
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: int, update: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            if update.title is not None:
+                task["title"] = update.title
+            if update.completed is not None:
+                task["completed"] = update.completed
+            return task
+    raise HTTPException(status_code=404, detail="Item not Found")
+
+@app.put("/tasks/{task_id}")
+def replace_task(task_id: int, update: TaskReplace):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = update.title
+            task["completed"] = update.completed
+            return task
+    raise HTTPException(status_code=404, detail="Item not Found")       
